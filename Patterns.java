df1ef988003cfb35062e9eb7f3fc367e3d2d0fb9
Vedant-Kohale1910 <116712218+Vedant-Kohale1910@users.noreply.github.com>
2024-10-17 10:51:53 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,17 @@
+package com.java;
+
+public class Patterns{
+    public static void triangle(int n){
+        int i,j;
+        for(i=0; i<=0; i++){
+            for(j=0; j<=i; j++){
+                System.out.print("");
+            }
+            System.out.println("*");
+        }
+    }
+    public static void main(String[] args){
+        int n = 4;
+        triangle(n);
+    }
+}
